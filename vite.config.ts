925afc57a3c295090f6b0d23f2cee1024f3c84e6
BeerSlothAgent <peterkrulis@proton.me>
2025-07-06 11:06:58 +0200

    Fix esbuild optimization errors

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    exclude: [
+      'hash.js',
+      'md5.js',
+      '@safe-global/safe-ethers-lib',
+      '@ethersproject/providers',
+      '@walletconnect/universal-provider',
+      'crypto-js',
+      '@eth-optimism/sdk',
+      'ethjs-unit'
+    ]
+  }
 })
